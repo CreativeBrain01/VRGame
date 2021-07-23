a31b8a4ba8be7d446af7e6fa48f98309c5e6386e
Anastasia Rapp <zmraxton@gmail.com>
2021-07-22 19:46:36 -0600

    Timer and crying
    Added a timer and spent an hour and a half crying over the gun being terrible and couldn't get it to work oops

---
+++ VRStuff/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager Instance { get => instance; }
+
+    private GameManager instance;
+
+    Vector3 startPosition;
+
+    public TMP_Text timerText;
+
+    [Min(0)] public float maxTimeSeconds;
+
+    private float timer;
+
+    public int minutes { get => (int)(timer / 60); }
+    public int seconds { get => (int)(timer % 60); }
+
+    void Start()
+    {
+        instance = this;
+        startPosition = gameObject.transform.position;
+        timer = maxTimeSeconds;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerText.text = $"{minutes} : {seconds}";
+    }
+}
